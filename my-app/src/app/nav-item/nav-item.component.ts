b18f9f5a33ca9aeaa75fb84b70215dc067d7ed21
AvishaiAt <avishaiat@gmail.com>
2017-03-13 19:43:17 +0200

    test3

--- my-app/src/app/nav-item/nav-item.component.ts
+++ my-app/src/app/nav-item/nav-item.component.ts
@@ -2,9 +2,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-litem',
-  templateUrl: './litem.component.html',
-  styleUrls: ['./litem.component.css']
+  selector: 'app-nav-item',
+  templateUrl: './nav-item.component.html',
+  styleUrls: ['./nav-item.component.css']
 })
 export class LitemComponent implements OnInit {
 
